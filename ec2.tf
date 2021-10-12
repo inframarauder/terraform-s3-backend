ca75648efc55367636a902b4600f957e92813698
subhasis020299 <subhasisdas125@gmail.com>
2021-10-12 13:48:42 +0530

    getting latest ubuntu ami using data source

--- ec2.tf
+++ ec2.tf
@@ -3,8 +3,24 @@ provider "aws" {
   profile = var.profile
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  
+  
+  filter {
+      name = "virtualization-type"
+      values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "ubuntu-server" {
-  ami           = var.ubuntu_image_id
+  ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
   tags = {
--- variables.tf
+++ variables.tf
@@ -7,10 +7,6 @@ variable "profile" {
   default = "default"
 }
 
-variable "ubuntu_image_id" {
-  type = string
-}
-
 variable "instance_type" {
   type    = string
   default = "t2.micro"
